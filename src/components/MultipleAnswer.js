0159e89becf1777d450518099d4eed1b9dd34902
Berkay <berkayerarslan96@gmail.com>
2022-04-04 17:48:43 +0300

    Çoktan seçmeli cevapları olan soruların yapımı

--- src/components/MultipleAnswer.js
+++ src/components/MultipleAnswer.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
-const MultipleAnswer = () => {
+const MultipleAnswer = (props) => {
+  
   return (
-    <div>MultipleAnswer</div>
+    <div className="test-answers" >
+    {props.data.checkedQuestion &&
+      props.data.checkedQuestion.answers.map((item, key) => {
+        
+        return (
+          <div className="test-answer">
+            <input
+              id={item.id}
+              key={key}
+              type= {props.type}
+              checked={ item.id === props.data.checkedOption?.answerId}
+              onClick={(e) => props.onClick(e)}   
+            />
+            <label htmlFor="">{item?.answer}</label>
+          </div>
+        );
+      })}
+  </div>
   )
 }
 
